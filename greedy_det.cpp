33785f356b4077a132d2a1589c00c5211d1ab5dd
Vicente Cser <vicentecser@gmail.com>
2023-09-12 21:36:06 -0300

    Implement greedy

--- greedy_det.cpp
+++ greedy_det.cpp
@@ -1,19 +1,24 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
-#include <fstream>
+#include <numeric>
+#include <climits>
 
 using namespace std;
 
-int main(int argc, char const *argv[]){
-
-    if(argc > 2){
-        if(argv[1][0] != '-' || argv[1][1] != 'i'){
-            cout<<"Debes ingresar el comando -i "<<endl;
+int main(int argc, char const *argv[]) {
+    if (argc > 2) {
+        if (argv[1][0] != '-' || argv[1][1] != 'i') {
+            cout << "Debes ingresar el comando -i " << endl;
             return 0;
         }
-    }else{
-        cout<<"Debes ingresar el formato de ejecución <Greedy> -i <instancia-problema>"<<endl;
+    } else {
+        cout << "Debes ingresar el formato de ejecución <Greedy> -i "
+                "<instancia-problema>"
+             << endl;
         return 0;
     }
     fstream file;
@@ -25,14 +30,80 @@ int main(int argc, char const *argv[]){
     if (file.is_open()) {
         string data;
         while (getline(file, data)) {
-            dataset.push_back(data);       
+            dataset.push_back(data);
         }
         file.close();
     }
-    for (int i = 0; i < dataset.size(); i++){
-        cout<<dataset[i]<<endl;
+    // for (int i = 0; i < dataset.size(); i++) {
+    //     cout << dataset[i] << endl;
+    // }
+    // cout << dataset.size() << endl;
+
+    vector<int> distances(dataset.size(), 0);
+    string result = "";
+    int cost = 0;
+
+    // elegir como primera letra la mas frecuente
+    map<char, int> m;
+    for (int i = 0; i < dataset.size(); i++) {
+        m[dataset[i][0]]++;
     }
-    cout<<dataset.size()<<endl;
+    auto best = max_element(m.begin(), m.end(), [](auto &a, auto &b) -> bool {
+        return a.second < b.second;
+    });
+
+    // cout << best->first << " " << best->second << endl;
+    result += best->first;
+
+    for (int i = 0; i < dataset.size(); i++) {
+        if (dataset[i][0] != best->first) {
+            distances[i]++;
+        }
+    }
+
+    for (auto value : distances) {
+        cost += value * value;
+    }
+    // cout << cost << endl;
+
+    // elegir siguientes letras
+    // cada columna
+    for (int i = 1; i < dataset[0].size(); i++) {
+        int min_cost = INT_MAX;
+        char best_letter = 'A';
+        vector<int> best_distances;
+        // cada letra
+        for (char letter : {'A', 'C', 'T', 'G'}) {
+            // cada fila
+            // copiar distancias
+            vector<int> distances_copy = distances;
+            cost = 0;
+            for (int sequence = 0; sequence < dataset.size(); sequence++) {
+                if (dataset[sequence][i] != letter) {
+                    distances_copy[sequence]++;
+                }
+                // cout << sequence << endl;
+            }
+            // cout << "hola" << endl;
+            for (auto value : distances_copy) {
+                cost += value * value;
+            }
+            // cout << "chao" << endl;
+            if (cost < min_cost) {
+                min_cost = cost;
+                best_letter = letter;
+                best_distances = distances_copy;
+            }
+        }
+        result += best_letter;
+        distances = best_distances;
+    }
+
+    cout << result << endl;
+    for (auto value : distances) {
+        cost += value * value;
+    }
+    cout << cost << endl;
     
     return 1;
 }
--- prueba.cpp
+++ prueba.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    cout<< "hola mundo "<<endl;   
+int main(int argc, char const *argv[]) {
+    cout << "hola mundo " << endl;
     return 0;
 }
